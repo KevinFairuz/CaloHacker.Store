86fa8fbf235152757532f7aead48cb265ef15984
KevinFairuz <kevin.10122346@mahasiswa.unikom.ac.id>
2023-08-02 22:27:34 +0700

    ganti title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import './globals.css';
 const font = Urbanist({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'caloHackerStore',
+  title: 'fiventhrift',
   description: 'Store - The place for all your purchases.',
 };
 
